84e94992ab6f4d8a16a0d3014ebf1a1d8ea18274
Nathan <natefintak@gmail.com>
2025-03-08 22:09:22 -0500

    getLastElement method finished

--- src/main/java/com/zipcodewilmington/StringArrayUtils.java
+++ src/main/java/com/zipcodewilmington/StringArrayUtils.java
@@ -25,7 +25,8 @@ public class StringArrayUtils {
      * @return last element in specified array
      */ // TODO
     public static String getLastElement(String[] array) {
-        return null;
+        return array[array.length - 1];
+        //return null;
     }
 
     /**
